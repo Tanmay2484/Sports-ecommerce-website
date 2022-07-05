5145fcf41260298a410b67cb0751bb560c0f57d5
Tanmay Dutta <103728522+Tanmay2484@users.noreply.github.com>
2022-07-05 16:21:16 +0530

    Add files via upload

---
+++ ecomerce website (Styler)/script.js
@@ -0,0 +1,16 @@
+
+let MenuItems=document.getElementById("MenuItems")
+MenuItems.style.maxHeight="0px"
+menutoggle=()=>{
+    if(MenuItems.style.maxHeight=="0px"){
+        MenuItems.style.maxHeight="200px"
+    }
+    else
+    {
+        MenuItems.style.maxHeight="0px"
+    }
+   
+}
+
+
+
